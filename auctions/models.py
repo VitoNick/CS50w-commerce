07f2851687f907de16e767c75462be1fc28b0855
VitoNick <vitonick81@gmail.com>
2025-10-10 20:28:20 -0700

    Add new migrations for AuctionListing, first migrate.

--- auctions/models.py
+++ auctions/models.py
@@ -6,7 +6,10 @@ class User(AbstractUser):
     pass
 
 class AuctionListing(models.Model):
-    pass
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=64)
+    starting_bid  = models.IntegerField()
+    image_url = models.URLField()
 
 class Bid():
     pass
